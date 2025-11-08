3af6a6d855b913d65ffd33ceacfa1fcc30496108
irshadkhan24 <irshakhan012003@gmail.com>
2025-11-08 13:08:00 +0530

    Update EmployeeHelper.jsx

--- src/utils/EmployeeHelper.jsx
+++ src/utils/EmployeeHelper.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
-//Column Concepts
+// Column Concepts
 export const columns = [
   {
       name: "S No",
@@ -17,13 +17,11 @@ export const columns = [
   {
     name: "Image",
     selector: (row) => row.profileImage,
-    //sortable: true,
     width: "90px"
   },
   {
     name: "Department",
     selector: (row) => row.dep_name,
-    //sortable: true,
     width: "120px"
   },
   {
@@ -35,70 +33,80 @@ export const columns = [
   {
       name: "Action",
       selector: (row) => row.action,
-      center: "true"
+      center: true
   },
 ];
 
+// fetch departments
 export const fetchDepartments = async () => {
-    let departments
+    let departments;
     try {
-      const responnse = await axios.get('https://employee-api-41gx.vercel.app/api/department', {
+      const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/department`, {
         headers: {
           Authorization : `Bearer ${localStorage.getItem('token')}`
         }
-      })
-      if(responnse.data.success) {
-        //console.log(responnse.data)
-        departments = responnse.data.departments
+      });
+      if(response.data.success) {
+        departments = response.data.departments;
       }
     } catch(error) {
+      console.error(error);
       if(error.response && !error.response.data.success) {
-        alert(error.response.data.error)
+        alert(error.response.data.error);
       }
     } 
-    return departments
-  };
+    return departments;
+};
 
-  // employees for salary form
-  export const getEmployees = async (id) => {
+// employees for salary form
+export const getEmployees = async (id) => {
     let employees;
     try {
-      const responnse = await axios.get(`https://employee-api-41gx.vercel.app/api/employee/department/${id}`, {
+      const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/employee/department/${id}`, {
         headers: {
           Authorization : `Bearer ${localStorage.getItem('token')}`
         },
       });
-      console.log(responnse)
-      if(responnse.data.success) {
-        employees = responnse.data.employees;
+      if(response.data.success) {
+        employees = response.data.employees;
       }
     } catch(error) {
+      console.error(error);
       if(error.response && !error.response.data.success) {
-        alert(error.response.data.error)
+        alert(error.response.data.error);
       }
     } 
-    return employees
-  };
+    return employees;
+};
 
+// Employee Buttons
+export const EmployeeButtons = ({Id}) => {
+    const navigate = useNavigate();
 
-  // Button concepts
-  export const EmployeeButtons = ({Id}) => {
-      const navigate = useNavigate();
-
-      return (
-          <div className="flex space-x-3">
-              <button 
+    return (
+        <div className="flex space-x-3">
+            <button 
               className="px-3 py-1 bg-teal-500 hover:bg-teal-600 text-white"
               onClick={() => navigate(`/admin-dashboard/employees/${Id}`)}
-              >View</button>
-              <button 
+            >
+              View
+            </button>
+            <button 
               className="px-3 py-1 bg-blue-500 hover:bg-blue-600 text-white"
               onClick={() => navigate(`/admin-dashboard/employees/edit/${Id}`)}
-              >Edit</button>
-              <button className="px-3 py-1 bg-amber-500 hover:bg-amber-600 text-white" 
-              onClick={() => navigate(`/admin-dashboard/employees/salary/${Id}`)}>Salary</button>
-              <button className="px-3 py-1 bg-rose-500 hover:bg-rose-600 text-white"
-              onClick={() => navigate(`/admin-dashboard/employees/leaves/${Id}`)}>Leave</button>
-          </div>
-      )
-  };
+            >
+              Edit
+            </button>
+            <button className="px-3 py-1 bg-amber-500 hover:bg-amber-600 text-white" 
+              onClick={() => navigate(`/admin-dashboard/employees/salary/${Id}`)}
+            >
+              Salary
+            </button>
+            <button className="px-3 py-1 bg-rose-500 hover:bg-rose-600 text-white"
+              onClick={() => navigate(`/admin-dashboard/employees/leaves/${Id}`)}
+            >
+              Leave
+            </button>
+        </div>
+    )
+};
